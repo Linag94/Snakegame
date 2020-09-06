c1cd5f68f6541c1c4529b995705f39c4af17c068
Thelina Geogre <George.thelina@gmail.com>
2020-09-06 08:39:40 -0400

    delete

--- javascript/wishlist.js
+++
@@ -1,7 +0,0 @@
-$("a").click(function() {
-    console.log("classname - " + this.className)
-    if(this.className=="" || this.className == "remove")
-        this.className = "add";
-    else 
-        this.className = "remove";
-  });
